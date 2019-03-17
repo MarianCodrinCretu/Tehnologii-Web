caf07b8ea678ab8f091609338596bb608fac5cd6
Iulian Bogdan GURALIUC <iguraliuc@bitdefender.com>
2019-03-17 12:58:30 +0200

    Improved movie list by adding javascript; Style modifications

---
+++ Main Page/application.js
@@ -0,0 +1,38 @@
+function init() {
+
+    var coll = document.getElementsByClassName("card-media");
+    var i;
+    console.log(coll);
+    console.log(coll.length);
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+            var content = this.nextElementSibling;
+            if (content.style.position === "absolute" || content.style.position.length == 0) {
+                closeAllMovies();
+                content.style.position = "relative";
+                content.style.top = 0;
+                content.style.left = 0;
+                console.log("hidden -> shown");
+
+            } else {
+                content.style.position = "absolute";
+                content.style.top = -9999;
+                content.style.left = -9999;
+                console.log("show -> hidden");
+            }
+        });
+    }
+
+    function closeAllMovies() {
+        var toClose = document.getElementsByClassName("card-body");
+
+        var j;
+        for (j = 0; j < toClose.length; j++) {
+            toClose[j].style.position = "absolute";
+            toClose[j].style.top = -9999;
+            toClose[j].style.left = -9999;
+        }
+    }
+
+}
+init();
