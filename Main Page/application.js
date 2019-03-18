7b153bb5c768f55f7a1afa5d06b513a227d55833
Cretu Marian-Codrin <cretu.marian.codrin@gmail.com>
2019-03-18 22:45:18 +0200

    validat + attempt js

--- Main Page/application.js
+++ Main Page/application.js
@@ -5,7 +5,7 @@ function init() {
     console.log(coll);
     console.log(coll.length);
     for (i = 0; i < coll.length; i++) {
-        coll[i].addEventListener("click", function() {
+        coll[i].addEventListener("mouseover", function() {
             var content = this.nextElementSibling;
             if (content.style.position === "absolute" || content.style.position.length == 0) {
                 closeAllMovies();
@@ -15,7 +15,8 @@ function init() {
                 console.log("hidden -> shown");
 
             } else {
-                content.style.position = "absolute";
+                col[i].style.position = "absolute";
+                
                 content.style.top = -9999;
                 content.style.left = -9999;
                 console.log("show -> hidden");
