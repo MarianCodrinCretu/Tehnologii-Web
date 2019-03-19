32647830a42ef9fc9cf8720ea47718118d39e6bb
Cretu Marian-Codrin <cretu.marian.codrin@gmail.com>
2019-03-19 17:13:06 +0200

    nevalidat

--- Main Page/application.js
+++ Main Page/application.js
@@ -5,7 +5,7 @@ function init() {
     console.log(coll);
     console.log(coll.length);
     for (i = 0; i < coll.length; i++) {
-        coll[i].addEventListener("mouseover", function() {
+        coll[i].addEventListener("click", function() {
             var content = this.nextElementSibling;
             if (content.style.position === "absolute" || content.style.position.length == 0) {
                 closeAllMovies();
@@ -15,8 +15,7 @@ function init() {
                 console.log("hidden -> shown");
 
             } else {
-                col[i].style.position = "absolute";
-                
+                content.style.position = "absolute";
                 content.style.top = -9999;
                 content.style.left = -9999;
                 console.log("show -> hidden");
